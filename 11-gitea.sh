268f7f3e5409030dcf909d238cfeec5a953ed4c9
Sami-Pekka Hallikas <semi@hallikas.com>
2026-09-08 10:38:27 +0300

    Fixed typos

--- 11-gitea.sh
+++ 11-gitea.sh
@@ -1,13 +1,13 @@
 #echo "Verify CLUSTER value in this file, and remove/comment this line" && exit
 CLUSTER=kube.semilab.net
 
-[ -e gitea.values.yaml ] || cat <<"EOF" > gitea.values.yaml
+[ -e gitea.values.yaml ] || cat <<EOF > gitea.values.yaml
 gitea:
   config:
     APP_NAME: "Demo Git"
     server:
-      DOMAIN: git.${CLUSTER}}
-      ROOT_URL: https://git.${CLUSTER}}/
+      DOMAIN: git.${CLUSTER}
+      ROOT_URL: https://git.${CLUSTER}/
       SSH_PORT: 2222 # rootful image
       SSH_LISTEN_PORT: 2222 # rootless image
     service:
@@ -78,6 +78,7 @@ spec:
 # volumeName: pv-gitea
 EOF
 kubectl apply -f gitea-pvc.yaml
+exit
 
 #helm show values oci://docker.gitea.com/charts/gitea > gitea.values.default.yaml
 helm upgrade --install gitea oci://docker.gitea.com/charts/gitea \
